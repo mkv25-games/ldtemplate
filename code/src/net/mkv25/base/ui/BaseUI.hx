cf6c468c0b4d03a27edc2d18a5ca438b2af6c5e1
John Beech <john.beech@bbc.co.uk>
2016-04-08 10:55:29 +0100

    Fix type consistency to work with new versions of Actuate and Openfl

--- code/src/net/mkv25/base/ui/BaseUI.hx
+++ code/src/net/mkv25/base/ui/BaseUI.hx
@@ -3,78 +3,78 @@ package net.mkv25.base.ui;
 import flash.display.DisplayObject;
 import flash.display.Sprite;
 import motion.Actuate;
-import motion.actuators.GenericActuator.IGenericActuator;
+import motion.actuators.IGenericActuator;
 import motion.easing.Elastic;
 import motion.easing.Quad;
 import net.mkv25.game.Index;
 import openfl.display.DisplayObjectContainer;
 import openfl.geom.Point;
 
-class BaseUI 
+class BaseUI
 {
 	public var artwork:Sprite;
 	var enabled:Bool;
-	
+
 	public var scale(get, set):Float;
 	public var visible(get, set):Bool;
 
-	public function new() 
+	public function new()
 	{
 		artwork = new Sprite();
 	}
-	
+
 	// public methods
 	public function move(x:Float, y:Float):BaseUI
 	{
 		artwork.x = x;
 		artwork.y = y;
-		
+
 		return this;
 	}
-	
+
 	public function moveToMouse():BaseUI
 	{
 		if (artwork.parent != null)
 		{
 			move(artwork.parent.mouseX, artwork.parent.mouseY);
 		}
-		
+
 		return this;
 	}
-	
+
 	function movePolar(angle:Float, radius:Float):BaseUI
 	{
 		var theta = angle / 180 * Math.PI;
 		var x = radius * Math.cos(theta);
 		var y = radius * Math.sin(theta);
-		
+
 		artwork.x = x;
 		artwork.y = y;
-		
+
 		return this;
 	}
-	
+
 	public function size(width:Float, height:Float):BaseUI
 	{
 		artwork.width = width;
 		artwork.height = height;
-		
+
 		return this;
 	}
-	
+
 	public function addTo(container:DisplayObjectContainer):BaseUI
 	{
 		container.addChild(this.artwork);
-		
+
 		return this;
 	}
-	
+
 	public function center(image:DisplayObject, offsetX:Int=0, offsetY:Int=0):Void
 	{
 		image.x = - Math.round(image.width / 2) + offsetX;
 		image.y = - Math.round(image.height / 2) + offsetY;
 	}
-	
+
 	public function enable()
 	{
 		enabled = true;
@@ -82,7 +82,7 @@ class BaseUI
 		artwork.mouseChildren = true;
 		artwork.alpha = 1.0;
 	}
-	
+
 	public function disable()
 	{
 		enabled = false;
@@ -90,42 +90,42 @@ class BaseUI
 		artwork.mouseChildren = false;
 		artwork.alpha = 0.4;
 	}
-	
+
 	public function show()
 	{
 		visible = true;
 	}
-	
+
 	public function hide()
 	{
 		visible = false;
 	}
-	
+
 	// animations
 	public function zoomOut():IGenericActuator
 	{
 		Actuate.apply(artwork, { alpha: 1.0, scaleX: 1.0, scaleY: 1.0 } );
 		return Actuate.tween(artwork, 0.6, { alpha: 0.0, scaleX: 2.0, scaleY: 2.0 } ).onComplete(hide);
 	}
-	
+
 	public function zoomIn():IGenericActuator
 	{
 		Actuate.apply(artwork, { alpha: 0.0, scaleX: 2.0, scaleY: 2.0 } );
 		return Actuate.tween(artwork, 0.6, { alpha: 1.0, scaleX: 1.0, scaleY: 1.0 } );
 	}
-	
+
 	public function popIn():IGenericActuator
 	{
 		Actuate.apply(artwork, { alpha: 0.0, scaleX: 0.5, scaleY: 0.5 } );
 		return Actuate.tween(artwork, 0.6, { alpha: 1.0, scaleX: 1.0, scaleY: 1.0 } );
 	}
-	
+
 	public function popOut():IGenericActuator
 	{
 		Actuate.apply(artwork, { alpha: 1.0, scaleX: 1.0, scaleY: 1.0 } );
 		return Actuate.tween(artwork, 0.6, { alpha: 0.0, scaleX: 0.5, scaleY: 0.5 } );
 	}
-	
+
 	private static var p1 = new Point();
 	private static var p2 = new Point();
 	public function moveBetween(source:BaseUI, target:BaseUI, animationLength:Float=0.8, delayTime:Float=0.0):IGenericActuator
@@ -134,44 +134,44 @@ class BaseUI
 		{
 			return null;
 		}
-		
+
 		var scale:Float = Index.screenController.scale;
-		
+
 		p1.x = 0;
 		p1.y = 0;
 		p1 = source.artwork.localToGlobal(p1);
 		p1.setTo(p1.x / scale, p1.y / scale);
-		
+
 		p2.x = 0;
 		p2.y = 0;
 		p2 = target.artwork.localToGlobal(p2);
 		p2.setTo(p2.x / scale, p2.y / scale);
-		
+
 		move(p1.x, p1.y);
-		
+
 		var targetx = artwork.x + (p2.x - p1.x);
 		var targety = artwork.y + (p2.y - p1.y);
-		
+
 		Actuate.tween(artwork, animationLength, { x: targetx } ).ease(Quad.easeIn).delay(delayTime);
 		return Actuate.tween(artwork, animationLength, { y: targety }, false ).ease(Quad.easeOut).delay(delayTime);
 	}
-	
+
 	// properties
 	public function get_scale():Float
 	{
 		return artwork.scaleX;
 	}
-	
+
 	public function set_scale(value:Float):Float
 	{
 		return artwork.scaleX = artwork.scaleY = value;
 	}
-	
+
 	public function get_visible():Bool
 	{
 		return artwork.visible;
 	}
-	
+
 	public function set_visible(value:Bool):Bool
 	{
 		return artwork.visible = value;
--- code/src/net/mkv25/base/ui/TextUI.hx
+++ code/src/net/mkv25/base/ui/TextUI.hx
@@ -10,124 +10,124 @@ import net.mkv25.base.core.Text;
 class TextUI extends BaseUI
 {
 	var text:TextField;
-	
+
 	public var textChanged:Signal;
 
-	public function new() 
+	public function new()
 	{
 		super();
 	}
-	
+
 	public function setup(label:String, color:Int=0x136713)
 	{
 		if (text == null)
 			text = Text.makeTextField("fonts/trebuc.ttf", 22, color, TextFormatAlign.CENTER, false);
-		
+
 		setText(label);
 		artwork.addChild(text);
-		
+
 		return this;
 	}
-	
+
 	public function getText():String
 	{
 		return text.text;
 	}
-	
+
 	public function setText(label:String)
 	{
 		text.text = label;
 	}
-	
+
 	public function align(alignment:Dynamic):TextUI
 	{
 		var format = text.defaultTextFormat;
 		format.align = alignment;
-		
+
 		text.defaultTextFormat = format;
 		text.setTextFormat(format);
-		
+
 		return this;
 	}
-	
+
 	public function alignLeft():TextUI
 	{
 		return align(TextFormatAlign.LEFT);
 	}
-	
+
 	public function alignRight():TextUI
 	{
 		return align(TextFormatAlign.RIGHT);
 	}
-	
+
 	public function alignCenter():TextUI
 	{
 		return align(TextFormatAlign.CENTER);
 	}
-	
+
 	public function alignJustify():TextUI
 	{
 		return align(TextFormatAlign.JUSTIFY);
 	}
 	
-	public function fontSize(size:Float):TextUI
+	public function fontSize(size:Int):TextUI
 	{
 		var format = text.defaultTextFormat;
 		format.size = size;
-		
+
 		text.defaultTextFormat = format;
 		text.setTextFormat(format);
-		
+
 		return this;
 	}
-	
+
 	public function leading(distance:Int):TextUI
 	{
 		var format = text.defaultTextFormat;
 		format.leading = distance;
-		
+
 		text.defaultTextFormat = format;
 		text.setTextFormat(format);
-		
+
 		return this;
 	}
-	
+
 	public function makeTextInput():TextUI
 	{
 		text.type = TextFieldType.INPUT;
 		text.selectable = true;
-		
+
 		artwork.mouseEnabled = true;
 		artwork.mouseChildren = true;
 		text.mouseEnabled = true;
-		
+
 		textChanged = new Signal();
 		text.addEventListener(Event.CHANGE, textChanged.dispatch);
-		
+
 		return this;
 	}
-	
+
 	public function maxChars(length:Int):TextUI
 	{
 		text.maxChars = length;
-		
+
 		return this;
 	}
-	
-	override public function size(width:Float, height:Float):BaseUI 
+
+	override public function size(width:Float, height:Float):BaseUI
 	{
 		text.width = width;
 		text.height = height;
-		
+
 		return this;
 	}
-	
+
 	public static function makeFor(label:String, color:Int=0x136713):TextUI
 	{
 		var text:TextUI = new TextUI();
-		
+
 		text.setup(label, color);
-		
+
 		return text;
 	}
 }
